c4a111f115fe9b713c400d390584e08044cce3e1
Daniel Lopez <ext_delopezp@falabella.cl>
2020-07-11 18:11:07 -0400

    Escribiendo tests para post, put y delete

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const axios = require('axios');
+const { users } = require('./endpoints');
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const usersHandlers = users({ axios });
+
+app.get('/', usersHandlers.get);
+
+app.post('/', usersHandlers.post);
+
+app.put('/:id', usersHandlers.put);
+
+app.delete('/:id', usersHandlers.delete);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
